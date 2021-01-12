a152e58ed8e0e56569c562604ef759a84837dc99
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-12 14:39:30 -0600

    added javascript objects

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,23 @@
+const fishCollection = [
+    {   
+        name: "Squishy",
+        species:"Cuttle fish ",
+        length:"1 ft",
+        location: "Mediterranean",
+        food: "fish"
+    }
+    ,{   
+        name: "Robin Hood",
+        species:"Silver Arrowona",
+        location: "Southeast Asia",
+        length:"3+ ft",
+        food: "fish"
+    }
+    ,{   
+        name: "yum-yum",
+        species:"Red-Bellied Piranha (Pygocentrus nattereri) ",
+        location: "Amazon River, Paraguay",
+        length:"5-10 inches",
+        food: "Fish, aquatic invertibrates,crustaceans"
+    }
+]
